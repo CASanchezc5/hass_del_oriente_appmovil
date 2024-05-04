f734a76e6ce99ddcfab469882d54c5193581be21
CASanchezc5 <cascqf@gmail.com>
2024-05-03 20:20:11 -0500

    se modifico frontPage.java para que cargue la pagina 3seg y luego pase a Login.java

--- app/src/main/java/com/example/hassdeloriente/frontPage.java
+++ app/src/main/java/com/example/hassdeloriente/frontPage.java
@@ -1,21 +1,26 @@
 package com.example.hassdeloriente;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class frontPage extends AppCompatActivity {
+public class frontPage  extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.frontpage_main);
 
+        // Usamos un Handler para posponer la ejecución del código
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Crea un Intent para iniciar la actividad Login
+                Intent intent = new Intent(frontPage.this, Login.class);
+                startActivity(intent);
+                finish();  // Finaliza la actividad actual para que el usuario no pueda volver a ella
+            }
+        }, 3000); // 3000 milisegundos = 3 segundos
     }
-
 }
