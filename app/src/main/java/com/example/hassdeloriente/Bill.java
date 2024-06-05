30da2e71fd0d1b9aa17dee31eafcd2d30a368bd7
sgcuadros17 <sgarcescu@cesde.net>
2024-06-04 23:41:58 -0500

    agrego bill

--- app/src/main/java/com/example/hassdeloriente/Bill.java
+++ app/src/main/java/com/example/hassdeloriente/Bill.java
@@ -8,7 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Bill extends AppCompatActivity {
+public class Bill
+
+        extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
